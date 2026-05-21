f95d69d75330fb0a163f1df86fed01dec6c7d42e
Mac792817 <792817797@qq.com>
2026-05-21 20:55:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,36 +10,32 @@ wss.on('connection', (ws) => {
     ws.on('message', (data) => {
         try {
             const msg = JSON.parse(data);
-            const { type, roomId } = msg;
+            const { roomId } = msg;
             if (!roomId) return;
 
             // 加入房间
-            if (type === "join") {
+            if (msg.type === "join") {
                 if (!rooms[roomId]) rooms[roomId] = [];
-                if (!rooms[roomId].includes(ws)) {
-                    rooms[roomId].push(ws);
-                }
+                rooms[roomId].push(ws);
             }
 
-            // 广播给房间里**所有人（包括自己房间的另一个人）**
-            if (rooms[roomId]) {
-                rooms[roomId].forEach(client => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(data);
-                    }
-                });
-            }
+            // 【关键】发给房间里所有人！！！
+            rooms[roomId].forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(data);
+                }
+            });
+
         } catch (e) {
-            console.error(e);
+            console.error("错误", e);
         }
     });
 
     ws.on('close', () => {
-        for (const roomId in rooms) {
-            rooms[roomId] = rooms[roomId].filter(client => client !== ws);
-            if (rooms[roomId].length === 0) delete rooms[roomId];
+        for (let id in rooms) {
+            rooms[id] = rooms[id].filter(c => c !== ws);
         }
     });
 });
 
-console.log("🎮 联机服务器启动");
+console.log("🎮 服务器已启动 - 双人联机");
